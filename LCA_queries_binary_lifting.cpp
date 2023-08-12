a31b6c31dca53b1ec3365d856e465baf7cf8f8fd
Lakshyadeep Patel <90641504+huehuehuehue69@users.noreply.github.com>
2023-08-13 01:16:24 +0530

    Update LCA_queries_binary_lifting.cpp

--- LCA_queries_binary_lifting.cpp
+++ LCA_queries_binary_lifting.cpp
@@ -1,94 +1,82 @@
-vector<vector<int>> adj;
-vector<vector<int>> dp;
-vector<int> depth;
-
-void build(vector<int>&par, int x, int n) {
+const int MAX = 200005;
+int dp[MAX][21];
+vector<int>adj[MAX];
+int depth[MAX];
+int par[MAX];
+void build(int x, int n) {
 	for (int i = 0; i < n; i++) {
 		dp[i][0] = par[i];
 	}
 	for (int j = 1; j <= x; j++) {
 		for (int i = 0; i < n; i++) {
-			if (dp[i][j - 1] == -1) {
-				dp[i][j] = -1;
-			}
-			else {
+			if (dp[i][j - 1] != -1) {
 				dp[i][j] = dp[dp[i][j - 1]][j - 1];
 			}
 		}
 	}
 }
-int find_kth(int s, int k, int x) {
-	for (int j = x; j >= 0; j--) {
-		if ((1 << j) & k) {
-			s = dp[s][j];
-			k -= (1 << j);
-			if (s == -1) {
-				return -1;
-			}
-		}
-	}
-	return s + 1;
-}
-void dfs(int u, int p, int d, vector<int>&depth) {
+void dfs(int u, int p, int d) {
 	depth[u] = d;
 	for (auto x : adj[u]) {
 		if (x != p) {
-			dfs(x, p, d + 1, depth);
+			par[x] = u;
+			dfs(x, u, d + 1);
 		}
 	}
 }
+int find_kth(int node, int k) {
+	int z = 0;
+	while (k > 0) {
+		if (k & 1) {
+			node = dp[node][z];
+		}
+		z++;
+		k >>= 1;
+	}
+	return node;
+}
+int lca(int a, int b, int x) {
+	if (depth[a] > depth[b]) {
+		swap(a, b);
+	}
+	int diff = depth[b] - depth[a];
+	b = find_kth(b, diff);
+	if (a == b) {
+		return a;
+	}
+	for (int j = x; j >= 0; j--) {
+		if (dp[a][j] != dp[b][j]) {
+			a = dp[a][j];
+			b = dp[b][j];
+		}
+	}
+	return dp[a][0];
+}
+
 void soluchan()
 {
 	int n, q;
 	cin >> n >> q;
-	vector<int>par(n);
-	par[0] = -1;
-	for (int i = 1; i < n; i++) {
-		cin >> par[i];
-		par[i]--;
+	for (int i = 0; i < n - 1; i++) {
+		int u, v;
+		cin >> u >> v;
+		u--;
+		v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
-	adj.resize(n);
-	for (int i = 1; i < n; i++) {
-		adj[par[i]].push_back(i);
-	}
-	// debug(adj);
-	// debug(par);
+	memset(par, -1, sizeof(par));
+	memset(depth, 0, sizeof(depth));
+	dfs(0, -1, 0);
 	int x = log2(n) + 1;
-	dp.resize(n , vector<int>(x + 1, -1));
-	build(par, x, n);
-	// debug(dp);
-	vector<int>depth(n, 0);
-	dfs(0, -1, 0, depth);
-	// debug(depth);
-	vector<int>res;
+	build(x, n);
 	while (q--) {
 		int a, b;
 		cin >> a >> b;
 		a--;
 		b--;
-		debug(depth[a]);
-		debug(depth[b]);
-		if (depth[a] < depth[b]) {
-			swap(a, b);
-		}
-		for (int i = x; i >= 0; i--) {
-			if ((depth[a] - pow(2, i)) >= depth[b]) {
-				a = dp[a][i];
-			}
-		}
-		if (a == b) {
-			res.push_back(a);
-			continue;
-		}
-		for (int j = x; j >= 0; j--) {
-			if (dp[a][j] != dp[b][j]) {
-				a = dp[a][j];
-				b = dp[b][j];
-			}
-		}
-		res.push_back(dp[a][0]);
-	}
-	for (auto x : res) {
-		cout << x + 1 << endl;
+		int w = lca(a, b, x);
+		int res = depth[a] + depth[b] - (2 * depth[w]);
+		cout << res << endl;
 	}
 }
